c941138b879f169cee2a72ceebdf17b3a49e3df4
Sergio Castelli <sersh@live.com.ar>
2021-09-26 23:21:08 -0300

    tarea

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "es6.js";
+
+function App() {
+    return <>
+             // Desarrolla tu implementación de un navbar dentro del componente NavBar.js
+             <div className="barra"> 
+                <NavBar />
+                <h2>Las ofertas de la semana</h2>
+                <ol>
+                    <li>asado 300</li>
+                    <li>cerdo 200</li>
+                    <li>pollo 100</li>
+                </ol>
+             </div>
+     </>;
+}
