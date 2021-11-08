e5046062a90b19eee801dc3eb24070ec6999c7e9
Sergio Castelli <sersh@live.com.ar>
2021-11-08 19:29:19 -0300

    tarea-9

--- src/components/item/itemlist.js
+++ src/components/item/itemlist.js
@@ -1,13 +1,21 @@
 import React from "react";
 import producto from './item'
+import cont from '../counter/itemCount'
 const CardList = () => {
-  
+  function onClick(evento){
+    suma = cont.precio 
+  }
     return (
+      
+      <>
       <ul>
         {bebida.map(u =>
           <li key={u.id}>{u.name}: {u.precio}</li>)}
       </ul>
-      
+      <div>
+        <p>suma: {suma}</p>
+      </div>
+      </>
     );
   };
   
